b494e5ccd68f70d8406ffc9383a88a6483fa8b6f
eric.keith <eric.keith@flatironschool.com>
2022-11-21 12:19:18 -0600

    commit tree 633a524f5cce5b4fc5b4f2db6046daf5eef9eec6

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 24;
 const message = `${name} is ${height} inches tall`;
 
+// Export Variables to other JS Code Files
 module.exports = {name, height, message};
+//console.log(message);
